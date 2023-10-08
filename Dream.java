40f19886b6a3511e0c65b63d145df6ec34251366
swapnilthakre468 <tswapnil468@gmail.com>
2023-10-08 17:54:17 +0530

    changes : this is from my heart

---
+++ Dream.java
@@ -0,0 +1,8 @@
+class Samples{
+	public static void main(Strig arg[]){
+
+System.out.println("I Will ...");
+	System.out.println("I have belief on me that I definitely achive what I dreamed ");
+
+}
+}
---
+++ Life.java
@@ -0,0 +1,11 @@
+class Life{
+	public static void main(Strig arg[]){
+
+        System.out.println("Thanks god giving such a beautiful life,");
+	System.out.println("thanks for giving such Good ,careing ,Hard working mom  i am ot done any good thing but she still love me  so thanks for that");
+	System.out.println("thanks for giving such Good ,careing ,Hard working dad who always with me and beleaving on me still i waste my lot of year so  thanks for that");
+	System.out.println("thanks for giving such Good ,careing ,sister who always with me and beleaving on me still when i slap her and fight with her so  thanks for that");
+	System.out.println("thanks for giving such Good ,cute , my friend ,my bestied  my pillu its my best gift i got from you god  so  thanks for that");
+
+}
+}
