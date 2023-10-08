c992fbf107cf327537e9da42f0b170f4365ef55c
swapnilthakre468 <tswapnil468@gmail.com>
2023-10-08 18:08:18 +0530

    changes :

--- Dream.java
+++ Dream.java
@@ -1,8 +1,8 @@
-class Samples{
+class Dream{
 	public static void main(Strig arg[]){
 
-System.out.println("I Will ...");
-	System.out.println("I have belief on me that I definitely achive what I dreamed ");
+        System.out.println("My Dream");
+	System.out.println(" My dream is to become person who can help any one whom ask me for help. and i also want to help the people from my side who are needy .i pray god please do my this dream complete. ");
 
 }
 }
